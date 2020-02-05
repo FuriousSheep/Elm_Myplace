80f754e8aaa66651e598e9b2004092e253f8afdd
Ioannis Katsaros <ioannis.katsaros@immobilier.email>
2020-02-05 17:20:41 +0100

    fix phone saving on email

--- src/InvitationWindow.elm
+++ src/InvitationWindow.elm
@@ -77,7 +77,7 @@ update msg model =
                     let isvalid = validateValue "\\+(9[976]\\d|8[987530]\\d|6[987]\\d|5[90]\\d|42\\d|3[875]\\d|2[98654321]\\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\\W*\\d\\W*\\d\\W*\\d\\W*\\d\\W*\\d\\W*\\d\\W*\\d\\W*\\d\\W*(\\d{1,2})$" model.typedPhone 
                     in
                     if isvalid then 
-                            ({ model | email = model.typedPhone}, Cmd.none)
+                            ({ model | phone = model.typedPhone}, Cmd.none)
                     else
                             ( model, Cmd.none)
 
